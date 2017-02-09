722bdceb86ffe220c941f1c2ee7742081052f521
Mckinley <mckinley.s.hill@gmail.com>
2017-02-09 20:27:39 +0000

    working with multer

--- api.js
+++ api.js
@@ -1,8 +1,19 @@
 var path = require('path');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'})
 
 module.exports = function(app){
     app.all('/', function(req,res){
         var filename = path.join(__dirname, 'index.html');
         res.sendFile(filename);
     });
+    
+    app.all('/get-file-size', upload.single('file'), function(req, res){
+        upload(req, res, function(err){
+          if(err){
+              console.log(err)
+          }  
+          res.send(req)
+        })
+    })
 };
